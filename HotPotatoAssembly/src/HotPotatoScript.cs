f2e453cb6f63c997e7b0c39480a9532fc4e64694
red031000 <rubenru09@aol.com>
2018-12-03 20:14:36 +0000

    initial commit

---
+++ HotPotatoAssembly/src/HotPotatoScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace HotPotatoAssembly
+{
+	public class HotPotatoScript : MonoBehaviour
+	{
+		private static int _moduleIdCounter;
+		private int _moduleId;
+		private KMNeedyModule _module;
+
+		private void Start()
+		{
+			_moduleId = _moduleIdCounter++;
+			_module = GetComponent<KMNeedyModule>();
+			_module.OnActivate += Activate;
+			_module.OnTimerExpired += TimerExpired;
+			_module.OnNeedyActivation += NeedyActivation;
+		}
+
+		private void Activate()
+		{
+			//bomb has been initialized
+		}
+
+		private void TimerExpired()
+		{
+			//timer has expired, better be on the table
+		}
+
+		private void NeedyActivation()
+		{
+			//needy has activated, show text
+		}
+	}
+}
