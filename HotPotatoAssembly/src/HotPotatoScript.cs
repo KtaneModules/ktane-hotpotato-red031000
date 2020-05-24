3ec1473ba81bebdc01a41cd845a6f84da8d1e7ff
MrPeanut1028 <50343979+MrPeanut1028@users.noreply.github.com>
2020-05-23 18:44:07 -0700

    Increase time to 15 seconds & Improve display for TP

--- HotPotatoAssembly/src/HotPotatoScript.cs
+++ HotPotatoAssembly/src/HotPotatoScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 namespace HotPotatoAssembly
 {
@@ -9,8 +10,12 @@ namespace HotPotatoAssembly
 		private KMNeedyModule _module;
 		private TextMesh _textMesh;
 		private FloatingHoldable _bomb;
+#pragma warning disable 0649
+        private readonly bool TwitchPlaysActive;
+#pragma warning restore 0649
+        private IEnumerator _TextU;
 
-		private void Start()
+        private void Start()
 		{
 			_moduleId = _moduleIdCounter++;
 			_module = GetComponent<KMNeedyModule>();
@@ -25,38 +30,51 @@ namespace HotPotatoAssembly
 		private void Activate()
 		{
 			_textMesh.text = string.Empty;
-		}
+            _TextU = TextUpdate();
+        }
 
 		private void TimerExpired()
 		{
 			if (_bomb.HoldState == FloatingHoldable.HoldStateEnum.Held)
 			{
 				_module.HandleStrike();
-				_textMesh.text = string.Empty;
 				Debug.LogFormat("[Hot Potato #{0}] Bomb not dropped in time, strike!", _moduleId);
 			}
 			else
 			{
 				_module.HandlePass();
-				_textMesh.text = string.Empty;
 				Debug.LogFormat("[Hot Potato #{0}] Bomb dropped in time.", _moduleId);
 			}
-		}
+            if (TwitchPlaysActive) StopCoroutine(_TextU);
+            _textMesh.text = string.Empty;
+        }
 
 		private void NeedyActivation()
 		{
-			_textMesh.text = "DROP THE\r\nBOMB";
+            if (TwitchPlaysActive) StartCoroutine(_TextU);
+            else _textMesh.text = "DROP THE\r\nBOMB";
 			Debug.LogFormat("[Hot Potato #{0}] Needy activated.", _moduleId);
 		}
 
 		private void NeedyDeactivation()
-		{
-			_textMesh.text = string.Empty;
+        {
+            if (TwitchPlaysActive) StopCoroutine(_TextU);
+            _textMesh.text = string.Empty;
 			Debug.LogFormat("[Hot Potato #{0}] Needy deactivated.", _moduleId);
 		}
-		
+
+        private IEnumerator TextUpdate()
+        {
+            while (true)
+            {
+                if (_bomb.HoldState == FloatingHoldable.HoldStateEnum.Held) _textMesh.text = "DROP THE\r\nBOMB";
+                else _textMesh.text = "BOMB\r\nDROPPED";
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
 #pragma warning disable 414
-		private readonly string TwitchHelpMessage = "Use !bomb drop to drop the bomb.";
+        private readonly string TwitchHelpMessage = "Use !bomb drop to drop the bomb.";
 #pragma warning restore 414
 		private KMSelectable[] ProcessTwitchCommand(string command)
 		{
